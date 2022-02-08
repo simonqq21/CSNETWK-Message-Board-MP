1f50df3dcc0bdb8bacacd7c1900f22499506c42d
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-02-09 01:16:05 +0800

    registration complete

--- udp_server.py
+++ udp_server.py
@@ -1,17 +1,27 @@
 import socket
+import json
 import sys
 
 
 listening_address = input("Enter listening IP address: ")
 listening_port = input("Enter listening port number: ")
 listening_port = int(listening_port)
+users = []
 
 # Create socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Server Starting up on %s port %d" % (listening_address, listening_port))
 
 # bind socket to the port
-s.bind((listening_address, listening_port))
+sock.bind((listening_address, listening_port))
 
 while True:
     # Waiting for data to arrive
-    data, address = s.recvfrom(1024)
+    print("\nServer has started. Waiting for new messages.")
+    data, address = sock.recvfrom(1024)
+    print(data)
+    temp = json.loads(data)
+
+    if data:
+        users.append(temp['username'])
+        print("Users in message board: ", users)
