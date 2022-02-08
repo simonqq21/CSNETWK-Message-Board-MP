22300bc23af930d68ac268fc5efa4bcfbc2feafc
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-02-08 23:45:46 +0800

    initial commit on server udp

--- udp_server.py
+++ udp_server.py
@@ -2,6 +2,16 @@ import socket
 import sys
 
 
-server_host = input("Enter listening IP address: ")
-dest_port = input("Enter listening port number: ")
-dest_port = int(dest_port)
+listening_address = input("Enter listening IP address: ")
+listening_port = input("Enter listening port number: ")
+listening_port = int(listening_port)
+
+# Create socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# bind socket to the port
+s.bind((listening_address, listening_port))
+
+while True:
+    # Waiting for data to arrive
+    data, address = s.recvfrom(1024)
