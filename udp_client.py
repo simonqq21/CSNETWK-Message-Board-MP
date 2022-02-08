1709515c8f082a4f24639727d621937824a95904
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-02-09 00:16:10 +0800

    removed indentation error, hardcoded IP and port values for testing

--- udp_client.py
+++ udp_client.py
@@ -14,20 +14,22 @@ codes = {201: "Command parameters incomplete",
 def register(username):
     register_cmd = {"command": "register", "username": username}
     jsondata = json.dumps(register_cmd)
-    try:
-        print(f"Registering username {username}")
-        sent = sock.sendto(bytes(jsondata, "utf-8"), (server_host, dest_port))
-        data, server = sock.recvfrom(1024)
-        print(codes[data["code_no"]])
-    
+    print(f"Registering username {username}")
+    sent = sock.sendto(bytes(jsondata, "utf-8"), (server_host, dest_port))
+    data, server = sock.recvfrom(1024)
+    print(codes[data["code_no"]])
+
 def deregister():
     pass
 
 def message():
     pass
 
-server_host = input("Enter IP address of message board server: ")
-dest_port = int(input("Enter port number of message board server: "))
+server_host = "172.16.11.59"
+dest_port = 12345
+# server_host = input("Enter IP address of message board server: ")
+# dest_port = int(input("Enter port number of message board server: "))
 username = input("Enter preferred username: ")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+register(username)
